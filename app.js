8d4cd03c24e0584ff01d719eaeff7f3442c6a55b
Manish Kumar Monu <92303219+Monu68@users.noreply.github.com>
2025-01-04 16:06:32 +0530

    captain registration done

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const app = express();
 const connectToDb = require('./db/db');
-const userRoutes = require('./routes/user.route')
+const userRoutes = require('./routes/user.route');
+const captainRoutes = require('./routes/captain.route');
 
 connectToDb()
 
@@ -20,5 +21,6 @@ app.get('/', (req, res) => {
 })
 
 app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
 
 module.exports = app;
